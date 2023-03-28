6ed6c195b9819c02947ebc1dacb2bc6dd8580bf5
Encryptedknox <100846845+Encryptedknox@users.noreply.github.com>
2023-03-28 12:36:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ db = Database(DATABASE_URL, "Telegraph-Uploader-Bot")
 
 Bot = Client(
     "Telegraph Uploader Bot",
-    bot_token=os.environ.get("BOT_TOKEN","5836641197:AAFn6rJE1tLGnbP947OK7wHLGhw26cBIxeM"),
+    bot_token=os.environ.get("BOT_TOKEN","6045837548:AAHEW6Ih1qN_2DgE75DJd4jQOump1aSM0gE"),
     api_id=int(os.environ.get("API_ID","15673531")),
     api_hash=os.environ.get("API_HASH","fc1b24e1970b13359d3f3e95b2d6de55")
 )
